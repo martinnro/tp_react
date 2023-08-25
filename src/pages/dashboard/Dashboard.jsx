1e42322e1cc0740f852c8a01a5a9fd7165a5e09f
Martin Romero Bazan <martinromero.bazan@gmail.com>
2023-08-25 16:45:14 -0300

    TP 2 Resolved

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -11,8 +11,11 @@ import {
 import { Link } from 'react-router-dom'; 
 import { Assignment, EventNote, ArrowForward as ArrowForwardIcon, AssignmentTurnedIn, AssignmentLate} from '@mui/icons-material';
 
+import { useSelector } from "react-redux";
+import { appSelector } from "../../redux/appRedux";
 
 const Dashboard = () => {
+    const todo = useSelector(appSelector.todo)
     return (
         <Grid container spacing={3}>
             <Grid item xs={12} md={6}>
@@ -39,6 +42,36 @@ const Dashboard = () => {
                         </Box>
                     </CardContent>
                     <Link to="/list">
+                        <IconButton>
+                            <ArrowForwardIcon />
+                        </IconButton>
+                    </Link>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <CardContent>
+                        <Box display="flex" alignItems="center">
+                            <AssignmentTurnedIn sx={{ fontSize: 40, marginRight: 2 }} />
+                            Tareas Completadas: {todo.filter(t=>t.completed).length}
+                        </Box>
+                    </CardContent>
+                    <Link to="/todo">
+                        <IconButton>
+                            <ArrowForwardIcon />
+                        </IconButton>
+                    </Link>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <CardContent>
+                        <Box display="flex" alignItems="center">
+                            <AssignmentLate sx={{ fontSize: 40, marginRight: 2 }} />
+                            Tareas Pendientes: {todo.length - todo.filter(t=>t.completed).length}
+                        </Box>
+                    </CardContent>
+                    <Link to="/todo">
                         <IconButton>
                             <ArrowForwardIcon />
                         </IconButton>
--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,23 +1,86 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import { v4 as uuid } from 'uuid';
+import { useSelector, useDispatch } from "react-redux";
+import { appSelector, appActions } from "../../redux/appRedux";
+
 import {
     Grid,
     Paper,
-    Box
+    Card,
+    CardHeader,
+    CardContent, 
+    Stack,
+    TextField,
+    Button,
+    Checkbox,
+    Typography
 }   from '@mui/material';
 
 
 const Todo = () => {
+    const dispatch = useDispatch()
+    const todo = useSelector(appSelector.todo)
+    const [text, setText] = useState(null)
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+        }
+
+        const handleChecked = (e, id) => {
+            dispatch(appActions.setCompletedTodo({id, completed:e.target.checked}))
+            }
+            
+        
+        const addTask = async () => {
+            dispatch(appActions.addTodo({text:text, id:uuid()}))
+            await setText(prev=>'')
+        }
+
+        const delTask = async (id) => {
+            dispatch(appActions.deleteTodo(id))
+            }
+
     return (
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 <Paper sx={{p: 2}}>
-                    <Box>
-                    Todo
-                    </Box>
+                    <Card>
+                        <CardHeader title="Agrega una tarea" />
+                        <CardContent>
+                            <Stack sx={{justifyContent:'space-around'}} direction='row'>
+                            <Grid item md={6}>
+                            <TextField value={text} label="tarea" variant="outlined" onChange={handleChange} />
+                            </Grid>
+                            <Grid item md={6}>
+                            <Button variant="contained" onClick={()=>addTask()}>Agregar</Button>
+                            </Grid>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader title="Tareas" />
+                        <CardContent>
+                        {todo.map((t, index)=>
+                        (
+                            <Stack key={t.id} sx={{justifyContent:'space-between'}} direction='row'>
+                                <Grid item md={1}>
+                                    <Checkbox checked = {t.completed} onChange={e=>handleChecked(e,t.id)} />
+                                </Grid>
+                                <Grid item md={9} sx={{pt: 1}}>
+                                    <Typography sx={{fontSize:18, fontWeight:700}}>{t.text}</Typography>
+                                </Grid>
+                                <Grid item md={2}>
+                                    <Button variant="contained" onClick={()=>delTask(t.id)}>Eliminar</Button>
+                                </Grid>
+                            </Stack>
+                        )
+                        )}
+                        </CardContent>
+                    </Card>
+
                 </Paper>
             </Grid>
         </Grid>
     );
 };
-
 export default Todo;
